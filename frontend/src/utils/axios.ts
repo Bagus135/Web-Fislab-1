d1bf6e846e8494f337ab361a9a175f63eb7d6da1
Bagus135 <bagusmustaqim35@gmail.com>
2024-08-26 02:26:16 +0700

    change

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios =  axios.create({
-baseURL : import.meta.env.MODE === "development" ? "http://localhost:5000" : import.meta.env.BASE_URL,
+baseURL : 'https://web-fislab-1-back.vercel.app/',
 withCredentials : true,
 })
 
