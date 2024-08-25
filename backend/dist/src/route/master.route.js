60d2a066686e206e7820397d33fe21f4cf5a8be8
Bagus135 <bagusmustaqim35@gmail.com>
2024-08-26 01:34:51 +0700

    a

--- backend/dist/src/route/master.route.js
+++
@@ -1,7 +0,0 @@
-import { editRole } from "../controller/master.controller.js";
-import express from "express";
-import tokenChecker from "../middleware/tokenChecker.js";
-import masterChecker from "../middleware/masterChecker.js";
-const masterRoutes = express.Router();
-masterRoutes.put('/role/:uid', tokenChecker, masterChecker, editRole);
-export default masterRoutes;
