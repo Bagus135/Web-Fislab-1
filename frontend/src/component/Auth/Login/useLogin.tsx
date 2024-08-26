44236f03c75faef514ed2fa583294e303e30d69e
Bagus135 <bagusmustaqim35@gmail.com>
2024-08-27 01:21:27 +0700

    omke gass

--- frontend/src/component/Auth/Login/useLogin.tsx
+++ frontend/src/component/Auth/Login/useLogin.tsx
@@ -3,6 +3,15 @@ import { useAuthContext } from "../../../context/AuthContext";
 import Axios from "../../../utils/axios";
 import toast from "react-hot-toast";
 
+function setCookie(na,value,days) {
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = na + "=" + (value || "")  + expires + "; path=/";
+}
 const useLogin = () =>{
     const[isLoading, setIsLoading] = useState<boolean>(false);
     const{setAuthUser} = useAuthContext();
@@ -17,9 +26,10 @@ const useLogin = () =>{
                 if(err.code === "ERR_NETWORK") throw new Error(err.message)
                 throw new Error(`${err.response.status} - ${err.response.statusText} \n ${err.response.data.error}`)
             })
+            setCookie('jwt', res.data.payload.token, 1)
             setAuthUser(res.data.payload)
             toast.success(res.data.message)
-            console.log(res.data.payload)
+            
         } catch (error:any){
             toast.error(error.message);
         } finally{
