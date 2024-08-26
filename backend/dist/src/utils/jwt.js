bcf7584fc1ed425ec4642d10f551396ff87b6605
Bagus135 <bagusmustaqim35@gmail.com>
2024-08-27 01:28:21 +0700

    a

--- backend/dist/src/utils/jwt.js
+++ backend/dist/src/utils/jwt.js
@@ -3,6 +3,9 @@ const generateToken = (userId, res) => {
     const token = jwt.sign({ userId }, process.env.jwtSecret, { expiresIn: '1d' });
     res.cookie('jwt', token, {
         maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly : true,
+        secure : true,
+        sameSite : 'none',
     });
     return token;
 };
--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -7,6 +7,7 @@ const generateToken = (userId :string , res : Response) =>{
         maxAge : 1 * 24* 60 * 60 * 1000,
         httpOnly : true,
         secure : true,
+        sameSite : 'none',
     })
     
 }
