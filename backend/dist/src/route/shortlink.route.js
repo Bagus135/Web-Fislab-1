82211105dcd361bea1832664463457f7653ec752
Bagus M <bagusmustaqim35@gmail.com>
2026-01-15 16:44:36 +0700

    add readme

--- backend/dist/src/route/shortlink.route.js
+++
@@ -1,9 +0,0 @@
-import { createShortLink, deleteShortlink, getAllShortLink } from "../controller/shortlink.controller.js";
-import express from "express";
-import tokenChecker from "../middleware/tokenChecker.js";
-import aslabChecker from "../middleware/aslabChecker.js";
-const ShortlinkRoutes = express.Router();
-ShortlinkRoutes.get("/all", getAllShortLink);
-ShortlinkRoutes.post("/:uid", tokenChecker, aslabChecker, createShortLink);
-ShortlinkRoutes.delete("/:id", tokenChecker, aslabChecker, deleteShortlink);
-export default ShortlinkRoutes;
